9fb6d7c1e42dee51a36e0914c9f9eb37dffa5ac8
Faith Hidalgo <imfaithangelou@gmail.com>
2022-08-15 07:33:48 -0700

    * updates for ENV and oth

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
-require('mongoose');
+let mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
